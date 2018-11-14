1c12269f4589407050b29193a4be9246107207ce
zhaolc90 <ytmssr@outlook.com>
2018-11-14 10:57:05 +0800

    Update a.ts

--- a.ts
+++ a.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import * as moment from 'moment';
 
 @Component({
@@ -9,56 +9,60 @@ import * as moment from 'moment';
 export class SscCalendarComponent implements OnInit {
 
   constructor() { }  
-  private _prefixCls = 'ssc-button';
-  private _today = new Date();
-  private _month;
-  private _moment: any;
-  private _selectedDay: Date;
+  private _prefixCls = 'ssc-calendar';
+
+  private _today = moment();
+  private _selected: any;
+  private _month: any = this._today.clone();
 
   currentClasses: {};
 
   @Input()
   set prefixCls(value: string) {
     this._prefixCls = value;
-    // this.setCurrentClasses();
+    // this.setCurrentClasses(); // todo
   }
-
   get prefixCls(): string {
     return this._prefixCls;
   }
 
   @Input()
-  set selectedDay(value: Date) {
-    this._selectedDay = value;
+  set selectedDay(value: any) {
+    if(value){
+      let m = value;
+      if(!moment.isMoment(value)){
+        m = moment(m);
+      }
+      if(m.isValid()){
+        this._month = m.clone();
+        this._selected = m.clone();
+      }
+    }
+  }
+  get selectedDay(): any {
+    return this._selected;
   }
 
-  get selectedDay(): Date {
-    return this._selectedDay;
+  @Output()
+  selectedDayChange = new EventEmitter<any>();
+
+  select(value: string) {
+    var date = this._month.clone().date(value)
+    this._selected = date
+    this.selectedDayChange.emit(date);
+    // this.didVote = true;
   }
 
-  @Input()
-  set setMonth(value) {
-    this._month = value;
-  }
-
-  prev() {
-    this._month = this._month.add(1, 'month');
+  jump(value: number) {
+    this._month = this._month.add(value, 'month');
   }
 
   get year(): string{
-    this._moment = moment(this._selectedDay?this._selectedDay:this._today);
-    if(this._month && this._month.isValid() !== undefined){
-      this._moment = this._month;
-    }
-    return this._moment.year();
+    return this._month.year();
   }
 
   get month(): string{
-    this._moment = moment(this._selectedDay?this._selectedDay:this._today);
-    if(this._month && this._month.isValid() !== undefined){
-      this._moment = this._month;
-    }
-    let month = this._moment.month();
+    let month = this._month.month();
     return moment.months()[month];
   }
 
@@ -66,40 +70,48 @@ export class SscCalendarComponent implements OnInit {
     return moment.weekdaysMin();
   }
 
-  get firstDay(): Date{
-    this._moment = moment(this._selectedDay?this._selectedDay:this._today);
-    let firstDayOfMonth = this._moment.clone().set('date', 1);    
-    return firstDayOfMonth.day(-1);
+  _firstDayOfMonth(): any{
+    return this._month.clone().set('date', 1);    
+  }
+  _lastDayOfMonth(): any{
+    return this._month.clone().set('date', 1).add(1, 'month').set('date', 0);    
+  }
+  _sameDay(a, b){
+    return (
+      a.year() == b.year()
+      && a.month() == b.month()
+      && a.date() == b.date()
+    )
   }
 
   get calendarDays(): number[]{
-    this._moment = moment(this._selectedDay?this._selectedDay:this._today);
-    if(this._month && this._month.isValid() !== undefined){
-      this._moment = this._month;
-    }
-    let firstDayOfMonth = this._moment.clone().set('date', 1);  
-    let lastDayOfMonth = this._moment.clone().add(1, 'month').set('date', 0);  
-    let start = firstDayOfMonth.clone().day(0)
-    let end = moment(start).day(34);
+    let monthStart = this._firstDayOfMonth();
+    let monthEnd = this._lastDayOfMonth();
+
+    let start = this._firstDayOfMonth().day(0);
+    let end = start.clone().day(41);
 
     let days = [];
     let day = start;
-    let i = 0, j, className:string
+    let i = 0, j, className: string
 
     while (day <= end) {
       days[i] = []
       for (j=0; j<7; j+=1) {
-        if (day < firstDayOfMonth){
+        if (day < monthStart){
           className = 'before'
         }
-        else if(day>lastDayOfMonth){
+        else if(day>monthEnd){
           className = 'after'
         }
-        else if(day.diff(this._today, 'days') == 0){
+        else if(this._sameDay(this._today, day)){
           className = 'today'
         }
         else {
           className = ''
+        }
+        if(this._selected && this._sameDay(this._selected, day)){
+          className = 'selected ' + className
         }
         days[i].push({date:day.date(), className: className});
         day = day.clone().add(1, 'd');
